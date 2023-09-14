5b6ae6a82cd3fa197d9fb46cb1959431b6f47f98
curtyraissa <raissacurty@gmail.com>
2023-09-14 01:08:31 +0100

    feat: Create Graphic component

---
+++ src/components/Graphic/Graphic.jsx
@@ -0,0 +1,13 @@
+import StyledGraphic from "./Graphic.styles";
+
+const Graphic = () => {
+    return (
+        <StyledGraphic>
+      <div className="graphic">
+        {/* Conteúdo gráfico será adicionado aqui */}
+      </div>
+      </StyledGraphic>
+    );
+  };
+  
+  export default Graphic;
