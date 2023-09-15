d54165b1ceb8b6429134b9f475b3c68fa167dbb1
curtyraissa <raissacurty@gmail.com>
2023-09-15 01:53:59 +0100

    feat: integration API on search component

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,11 @@ import { Title } from "./components/Title/Title";
 import { Overview } from "./components/Overview/Overview";
 import { Graphic } from "./components/Graphic/Graphic";
 
-// Dados fixos para o layout
-const objOverview = [
-  {
-    cityName: "Nome_da_Cidade",
-    minTemperature: "XX°C",
-    maxTemperature: "YY°C",
-    weatherCondition: "Nublado",
-    currentTemperature: "ZZ°C",
-  },
-];
-
 function App() {
   return (
     <div className="card">
       <Title />
       <Search />
-
-      {objOverview.map((item) => (
-        <Overview
-          key={item.cityName}
-          cityName={item.cityName}
-          minTemperature={item.minTemperature}
-          maxTemperature={item.maxTemperature}
-          weatherCondition={item.weatherCondition}
-          currentTemperature={item.currentTemperature}
-        />
-      ))}
       <Graphic />
     </div>
   );
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledSearch, Input, Button } from "./SearchStyles";
+import axios from "axios";
+import { Overview } from "../Overview/Overview";
+import { translateWeatherCondition } from "../../util/translateWeatherCondition";
 
 export const Search = () => {
+  const [city, setCity] = useState(""); // Estado para armazenar a cidade digitada
+  const [cityData, setCityData] = useState(null); // Estado para armazenar os dados da cidade
+
+  const apiKey = "8d0416a7768b0006840da31e0af379a9";
+
+  const handleSearch = () => {
+    if (city) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+        )
+        .then((response) => {
+          setCityData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching city data: ", error);
+        });
+    }
+  };
+
+  console.log("Weather Condition:", cityData?.weather[0]?.main);
+
   return (
-    <StyledSearch>
-      <Input type="text" placeholder="Digite o nome da cidade" />
-      <Button>Buscar</Button>
-    </StyledSearch>
+    <>
+      <StyledSearch>
+        <Input
+          type="text"
+          placeholder="Digite o nome da cidade"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <Button onClick={handleSearch}>Buscar</Button>
+      </StyledSearch>
+
+      {cityData && (
+        <Overview
+          cityName={cityData?.name}
+          weatherCondition={translateWeatherCondition(
+            cityData?.weather[0]?.main
+          )}
+          minTemperature={cityData?.main?.temp_min + "°C"}
+          maxTemperature={cityData?.main?.temp_max + "°C"}
+          currentTemperature={cityData?.main?.temp + "°C"}
+        />
+      )}
+    </>
   );
 };
