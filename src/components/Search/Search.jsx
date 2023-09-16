a604919b20a755cfac2e9c07e7a0c3cc609a67cb
curtyraissa <raissacurty@gmail.com>
2023-09-16 23:05:54 +0100

    feat: add graphic in component Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,29 +3,59 @@ import { StyledSearch, Input, Button } from "./SearchStyles";
 import axios from "axios";
 import { Overview } from "../Overview/Overview";
 import { translateWeatherCondition } from "../../util/translateWeatherCondition";
+import { Graphic } from "../Graphic/Graphic";
 
 export const Search = () => {
   const [city, setCity] = useState(""); // Estado para armazenar a cidade digitada
-  const [cityData, setCityData] = useState(null); // Estado para armazenar os dados da cidade
+  const [cityData, setCityData] = useState(null); // Estado para armazenar os dados da cidade na rota weather
+  const [forecastData, setForecastData] = useState(null); // Estado para armazenar os dados da cidade na rota forecast
 
   const apiKey = "8d0416a7768b0006840da31e0af379a9";
 
-  const handleSearch = () => {
-    if (city) {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
-        )
-        .then((response) => {
-          setCityData(response.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching city data: ", error);
-        });
+  const handleSearch = async () => {
+    try {
+      if (!city) {
+        return; // Não faz nada se 'city' não estiver definida
+      }
+
+      const [cityResponse, forecastResponse] = await Promise.all([
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        ),
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
+        ),
+      ]);
+
+      setCityData(cityResponse.data);
+
+      const chartData = forecastResponse.data.list.map(
+        ({ main: { temp }, dt_txt }) => {
+          const date = new Date(dt_txt);
+
+          return {
+            temp,
+            date: `${date.getDate()}/${date.getMonth() + 1}`, //TODO: Ajustar data
+          };
+        }
+      );
+
+      setForecastData(chartData);
+    } catch (error) {
+      if (error.response) {
+        // Erro de resposta da API (por exemplo, erro 404)
+        console.error("Erro na resposta da API:", error.response.data);
+      } else if (error.request) {
+        // Erro na requisição para a API (por exemplo, falha de rede)
+        console.error("Erro na requisição para a API:", error.request);
+      } else {
+        // Erro desconhecido
+        console.error("Erro desconhecido:", error.message);
+      }
     }
   };
 
-  console.log("Weather Condition:", cityData?.weather[0]?.main);
+  console.log("graaaaafico:", forecastData);
 
   return (
     <>
@@ -50,6 +80,8 @@ export const Search = () => {
           currentTemperature={cityData?.main?.temp + "°C"}
         />
       )}
+
+      {forecastData && <Graphic data={forecastData} />}
     </>
   );
 };
