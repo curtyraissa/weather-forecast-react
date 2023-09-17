a59fcdffb3d6f83eff5f719ebcc4695aac6ea8a3
curtyraissa <raissacurty@gmail.com>
2023-09-17 19:34:59 +0100

    style: graphic layout adjustment

--- src/components/Graphic/Graphic.jsx
+++ src/components/Graphic/Graphic.jsx
@@ -12,26 +12,26 @@ import {
 export const Graphic = ({ data }) => {
   return (
     <LineChart
-      width={500}
+      width={700}
       height={300}
       data={data}
       margin={{
         top: 5,
-        right: 30,
-        left: 20,
+        right: 10,
+        left: -30,
         bottom: 5,
       }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
+      <CartesianGrid strokeDasharray="3 3" /> {/* riscos do fundo r*/}
       <XAxis dataKey="date" />
       <YAxis />
-      <Tooltip />
+      <Tooltip /> {/* linha do cursor */}
       <Legend />
       <Line
         type="monotone"
         dataKey="temp"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
+        stroke="#8884d8" // cor da linha
+        activeDot={{ r: 8 }} // tamanho da bolinha selecionada
       />
     </LineChart>
   );
--- src/components/Graphic/GraphicStyles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const StyledGraphic = styled.div`
-  background-color: #fefefe;
-`;
